4a07ea5b99e4fd9ed805f84765c08c30b35101eb
Yang Ye <leafyoung@gmail.com>
2024-07-07 13:59:53 +0000

    3a-unique_ptr

---
+++ 3a-unique_ptr/main.cpp
@@ -0,0 +1,116 @@
+// https://replit.com/@YeKunlun/3a-uniqueptr?v=1
+
+#include <array>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+void f(int &t) { t += 3; }
+
+// receive ownership
+void f2(unique_ptr<int> p) { *p += 5; }
+
+auto f3(unique_ptr<int> p) {
+  *p += 7;
+  // transfer ownership back
+  return p;
+}
+class Widget {
+  mutable unique_ptr<int> data;
+
+public:
+  Widget(int x) : data{make_unique<int>(x)} {}
+  int get() { return *data; }
+  // Widget(const Widget &other) : data(nullptr) { other.data.swap(data); }
+  // Widget(const Widget &other) : data(make_unique<int>(*other.data)) {}
+};
+
+unique_ptr<int> return_int() { return make_unique<int>(3); }
+
+int main() {
+  {
+    // 1. Initialization solo and in container.
+    auto p = make_unique<int>(3);
+    auto p_arr = make_unique<array<int, 3>>();
+    (*p_arr)[0] = 0;
+    (*p_arr)[1] = 1;
+    (*p_arr)[2] = 2;
+    auto p_vec = make_unique<vector<int>>(3);
+    (*p_vec)[0] = 0;
+    (*p_vec)[1] = 1;
+    (*p_vec)[2] = 2;
+  }
+
+  {
+    // 2. Pass unique_ptr in/out function.
+
+    // not preferred, use make_unique
+    unique_ptr<int> p1{new int(1)};
+
+    // preferred
+    unique_ptr<int> p2 = make_unique<int>(3);
+    // or, use auto
+    auto p2a = make_unique<int>(3);
+
+    cout << "p1 (before): " << p1.get() << "\n";
+    p2a = move(p1);
+    // p2a's resource is released
+    // p1's object is transferred to p2a
+    // p1 is now nullptr
+    // *p2a is 1
+    cout << "p1 (after): " << p1.get() << "\n";
+
+    // Pass the value to a function.
+    f(*p2);
+    cout << *p2 << "\n";
+    cout << "p2: " << p2.get() << "\n";
+
+    // f2(p2); // error, p2 is not copyable
+    //  Use move to transfer ownership
+    f2(move(p2)); // ok, p2 is moved to f2
+    // p2 has become nullptr
+    cout << "p2 (after): " << p2.get() << "\n";
+
+    // move it in and receive it back
+    p2a = f3(move(p2a));
+    cout << *p2a << "\n";
+    p2a.reset();
+    cout << "p2a (after reset): " << p2a.get() << "\n";
+  }
+
+  {
+    // 3. unique_ptr in vector
+    vector<unique_ptr<int>> vs;
+    vs.emplace_back(make_unique<int>(3));
+    vs.emplace_back(make_unique<int>(4));
+    vs.emplace_back(make_unique<int>(5));
+
+    // if you have a pointer outside, use `move`
+    auto e4 = make_unique<int>(6);
+    vs.emplace_back(move(e4));
+
+    // int e5{6}; This will not work.
+    // vs.emplace_back(move(e5));
+
+    // auto v: vs cannot compile
+    // because it will copy the value from elements in vs to v.
+    // use reference (&) avoid copying
+    for (auto const &v : vs) {
+      cout << *v << ", ";
+    }
+    cout << "\n";
+  }
+
+  Widget w{315};
+  cout << w.get() << "\n";
+
+  {
+    cout << "return unique_ptr from a function"
+         << "\n";
+    auto x = return_int();
+    cout << *x << "\n";
+    cout << x.get() << "\n";
+  }
+}
