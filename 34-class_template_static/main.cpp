164922df13ae31ee67d1070ae47517d3bc001414
Yang Ye <leafyoung@gmail.com>
2024-07-07 13:46:55 +0000

    34-class_template_static

---
+++ 34-class_template_static/main.cpp
@@ -0,0 +1,24 @@
+#include <array>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T> class Widget {
+  inline static int unsigned long counter = 0;
+
+public:
+  Widget() { counter++; }
+  ~Widget() { counter--; }
+  auto getCounter() { return counter; }
+};
+
+int main() {
+  Widget<int> a;
+  Widget<int> b;
+  Widget<double> c;
+  cout << a.getCounter() << "\n"; // 2
+  cout << c.getCounter() << "\n"; // 1
+
+  // Template speciailization, like Widget<int> or Widget<double> can be seen as
+  // a standalone class.
+}
