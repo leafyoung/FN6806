c4a35b72cd5ca7a56d116825245fe6ef13ecb658
Yang Ye <leafyoung@gmail.com>
2026-04-17 00:38:44 +0800

    22 update

--- 22-operator/main.cpp
+++ 22-operator/main.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
-#include <numeric> // for std::accumulate
+#include <numeric>  // for std::accumulate
 using namespace std;
 
 struct MultiplyAndAdd {
@@ -20,7 +20,7 @@ struct KeepAdd {
 class MyBool {
   int x;
 
-public:
+ public:
   MyBool(int x1) : x{x1} {}
   operator bool() const { return x == 0; }
 };
@@ -29,23 +29,21 @@ int main() {
   // Stateful function with callable class
   {
     auto varr = std::array<int, 3>{{2, 3, 4}};
-    int factorial =
-        std::accumulate(varr.begin(), varr.end(), 1, MultiplyAndAdd{});
-    cout << factorial << '\n'; // 40
-    // accumulate: acc=1, op(1,2)=2, op(2,3)=8, op(6,4)=40
+    int factorial = std::accumulate(varr.begin(), varr.end(), 1, MultiplyAndAdd{});
+    cout << factorial << '\n';  // 40
+    // accumulate: acc=1, op(1,2)=1*2, op(2,3)=2+2*3=8, op(8,4)=8+8*4=40
 
     KeepAdd keepadd;
-    for_each(varr.begin(), varr.end(),
-             [keepadd = keepadd](auto x) { keepadd(x); });
-    cout << keepadd.sum << '\n'; // 9
+    for_each(varr.begin(), varr.end(), [keepadd = keepadd](auto x) { keepadd(x); });
+    cout << keepadd.sum << '\n';  // 9
     for_each(varr.begin(), varr.end(), keepadd);
-    cout << keepadd.sum << '\n'; // 18
+    cout << keepadd.sum << '\n';  // 18
   }
 
   {
     MyBool my_bool{3};
     // calls operator bool(), b = false
     bool b = static_cast<bool>(my_bool);
-    cout << b << endl;
+    cout << b << '\n';
   }
 }
