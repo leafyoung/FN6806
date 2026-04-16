82cb21062bd2a39b5b76e720c100eadcbc836689
Yang Ye <leafyoung@gmail.com>
2026-04-16 06:23:30 +0000

    52 update with eigen3

--- 52-mc_gbm/gbm_multi_thread_eigen.h
+++ 52-mc_gbm/gbm_multi_thread_eigen.h
@@ -1,13 +1,14 @@
 #pragma once
 
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
 
 #include "gbm_multi.h"
 
 using namespace Eigen;
 
-MatrixXd gbm_multipath_opt_thread_eigen(const GBMParam& gbm, const MCParam& mc, const Market& mkt,
-                                        const Eval& eval, const int& n_thread);
+MatrixXd gbm_multipath_opt_thread_eigen(const GBMParam &gbm, const MCParam &mc,
+                                        const Market &mkt, const Eval &eval,
+                                        const int &n_thread);
 
 // https://stackoverflow.com/questions/18400596/how-can-a-eigen-matrix-be-written-to-file-in-csv-format
 #include <fstream>
@@ -15,12 +16,14 @@ MatrixXd gbm_multipath_opt_thread_eigen(const GBMParam& gbm, const MCParam& mc, 
 
 // define the format you want, you only need one instance of this...
 // see https://eigen.tuxfamily.org/dox/structEigen_1_1IOFormat.html
-const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
+const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision,
+                                       Eigen::DontAlignCols, ", ", "\n");
 
 // writing functions taking Eigen types as parameters,
 // see https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html
 template <typename Derived>
-void writeToCSVfile(std::string name, const Eigen::MatrixBase<Derived>& matrix) {
+void writeToCSVfile(std::string name,
+                    const Eigen::MatrixBase<Derived> &matrix) {
   std::ofstream file(name.c_str());
   file << matrix.format(CSVFormat);
   // file.close() is not necessary,
