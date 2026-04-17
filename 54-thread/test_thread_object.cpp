5113edb9c159f0b08bbb9cb1b13ea1375eebb555
Yang Ye <leafyoung@gmail.com>
2026-04-17 17:10:10 +0800

    54 update

--- 54-thread/test_thread_object.cpp
+++ 54-thread/test_thread_object.cpp
@@ -6,7 +6,7 @@ using std::thread;
 class ForThread {
   int input, output;
 
-public:
+ public:
   ForThread(int input) : input(input), output(0) {}
   int add(int add) { return output = input + add; }
   int get_output() { return output; }
@@ -19,14 +19,4 @@ void test_launch() {
   thread t2(&ForThread::add, &for_thread, 4);
   t2.join();
   cout << for_thread.get_output() << "\n\n";
-  /*
-  {
-  cout << "ForThread\n";
-  ForThread for_thread(3);
-  // It will run a copy of for_thread, can not update for_thread
-  thread t(&for_thread, 4);
-  t.join();
-  cout << "g: " << for_thread.get_output() << '\n';
-  }
-  */
 }
