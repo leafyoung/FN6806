01cbdbb45249c0feca333db21c338cd1cf240e09
Yang Ye <leafyoung@gmail.com>
2026-04-17 23:25:30 +0800

    72 update

--- 72-thread-mtx-cv/task.h
+++ 72-thread-mtx-cv/task.h
@@ -6,7 +6,7 @@
 #include <stdexcept>
 
 struct Task {
-public:
+ public:
   const std::function<void(void)> task;
   const std::chrono::milliseconds sleep_in_milliseconds;
   const std::chrono::steady_clock::time_point start_time, wake_time;
@@ -16,19 +16,16 @@ public:
         sleep_in_milliseconds(std::chrono::milliseconds(sleep_in_ms)),
         start_time(std::chrono::steady_clock::now()),
         wake_time(std::chrono::steady_clock::now() + sleep_in_milliseconds) {
-
     if (sleep_in_milliseconds.count() < 0)
       throw std::logic_error("sleep_in_ms shall be > 0");
   }
 
-  friend std::ostream &operator<<(std::ostream &out, const Task &task) {
+  friend std::ostream& operator<<(std::ostream& out, const Task& task) {
     out << "sleep: " << task.sleep_in_milliseconds.count() << ", time range: "
-        << std::chrono::duration_cast<std::chrono::milliseconds>(
-               task.start_time.time_since_epoch())
+        << std::chrono::duration_cast<std::chrono::milliseconds>(task.start_time.time_since_epoch())
                .count()
         << '-'
-        << std::chrono::duration_cast<std::chrono::milliseconds>(
-               task.wake_time.time_since_epoch())
+        << std::chrono::duration_cast<std::chrono::milliseconds>(task.wake_time.time_since_epoch())
                .count();
 
     return out;
