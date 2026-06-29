36b9881a3d7996d9fef703579427450969f24fe9
Yang Ye <leafyoung@gmail.com>
2026-06-29 06:57:13 +0000

    52 post-fix

--- 52-mc_gbm/gbm_multi_thread_eigen.cpp
+++ 52-mc_gbm/gbm_multi_thread_eigen.cpp
@@ -2,6 +2,7 @@
 
 #ifdef HAS_EIGEN
 
+#include "split_mix64.h"
 #include <algorithm>
 #include <functional>
 #include <future>
@@ -10,15 +11,15 @@
 #include <memory>
 #include <random>
 #include <thread>
-#include "split_mix64.h"
 
 using namespace std;
 using namespace Eigen;
 
 using nd_double = normal_distribution<double>;
 
-void gbm_multipath_opt_inc_eigen(const GBMParam& gbm, const MCParam& mc, const Market& mkt,
-                                 const Eval& eval, MatrixXd& v, int start, int end) {
+void gbm_multipath_opt_inc_eigen(const GBMParam &gbm, const MCParam &mc,
+                                 const Market &mkt, const Eval &eval,
+                                 MatrixXd &v, int start, int end) {
   const auto drift = (gbm.mu - gbm.sigma * gbm.sigma / 2.0) * mc.dt;
   const auto diffusion = sqrt(mc.dt) * gbm.sigma;
   // nd is local (not static) so each thread owns its own instance.
@@ -36,8 +37,9 @@ void gbm_multipath_opt_inc_eigen(const GBMParam& gbm, const MCParam& mc, const M
   }
 }
 
-MatrixXd gbm_multipath_opt_thread_eigen(const GBMParam& gbm, const MCParam& mc, const Market& mkt,
-                                        const Eval& eval, size_t n_thread) {
+MatrixXd gbm_multipath_opt_thread_eigen(const GBMParam &gbm, const MCParam &mc,
+                                        const Market &mkt, const Eval &eval,
+                                        size_t n_thread) {
   uniform_int_distribution<uint64_t> uid;
   SplitMix64 sm(uid(mc.gen));
 
@@ -74,15 +76,15 @@ MatrixXd gbm_multipath_opt_thread_eigen(const GBMParam& gbm, const MCParam& mc, 
     threads.emplace_back(std::move(tasks.back()));
   }
 
-  for (auto& f : futures) {
+  for (auto &f : futures) {
     f.wait();
   }
 
-  for (auto& t : threads) {
+  for (auto &t : threads) {
     t.join();
   }
 
   return vs;
 }
 
-#endif  // HAS_EIGEN
+#endif // HAS_EIGEN
